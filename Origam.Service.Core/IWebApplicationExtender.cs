d19104dcf242099300b473a304526bbe0c1db98f
Jindrich Susen <jindrich.susen@advantages.cz>
2023-01-20 14:56:42 +0100

    + IWebApplicationExtender

---
+++ Origam.Service.Core/IWebApplicationExtender.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace Origam.Service.Core;
+
+public interface IWebApplicationExtender
+{
+    void Extend(IApplicationBuilder app, IConfiguration configuration);
+}
